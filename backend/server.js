96d735000f2d7435e409be28b8cd66cdcb53b836
Shamsher Khan <shamsherkhan7549@gmail.com>
2025-09-10 23:45:55 -0700

    feat: add backend server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
+const express = require('express');
 
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT||8080;
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('api working...');
+});
+
+app.listen(port, () => {
+    console.log(`server running on port ${port}`)
+})
