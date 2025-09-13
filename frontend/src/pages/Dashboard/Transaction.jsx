99e7d2f3bdd780306968ab77eaeb1ebef7fe81a3
Shamsher Khan <shamsherkhan7549@gmail.com>
2025-09-13 04:45:25 -0700

    feat: add delete and update feature in frontend

--- frontend/src/pages/Dashboard/Transaction.jsx
+++ frontend/src/pages/Dashboard/Transaction.jsx
@@ -1,13 +1,70 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { TransactionContext } from '../../context/ContextProvider'
+import { Button, Modal, Form, Input, Select } from 'antd';
 import './Dashboard.css'
+import { toast } from 'react-toastify';
+import axios from 'axios';
 
 const Transaction = () => {
-    const { transactionsInfo, setTransactionsInfo, fetchTransactions } = useContext(TransactionContext);
+    const { transactionsInfo, fetchTransactions } = useContext(TransactionContext);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [form] = Form.useForm();
+    const [editId, setEditId] = useState("")
+    const url = import.meta.env.VITE_BACKEND_URL;
 
-    useEffect(() => {
-        console.log(transactionsInfo);
-    }, [transactionsInfo])
+    const showModal = async (id) => {
+        try {
+            setIsModalOpen(true);
+            setEditId(id)
+            const response = await axios.get(`${url}/${id}`);
+            if (response.data.success) {
+                form.setFieldsValue(response.data.transaction);
+            }
+
+        } catch (error) {
+            toast.error(error.response.data.message || "Something went wrong");
+        }
+    };
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleSubmit = async (values) => {
+       
+        try {
+            const response = await axios.put(`${url}/${editId}`, values);
+            if (response.data.success) {
+                toast.success("Transaction updated successfully!");
+                fetchTransactions();
+                setIsModalOpen(false);
+                form.resetFields();
+
+            } else {
+                toast.error(response.data.message || "Something went wrong");
+            }
+        }
+        catch (error) {
+            toast.error(error.response.data.message || "Something went wrong");
+        }
+    }
+
+
+    // Delete transaction
+    const handlingDelete = async (id) => {
+        try {
+            const response = await axios.delete(`${url}/${id}`);
+            console.log(response);
+            if (response.data.success) {
+                toast.success(response.data.message || "Transaction deleted successfully!");
+                fetchTransactions();
+            } else {
+                toast.error(response.data.message || "Something went wrong");
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.response.data.message || "Error deleting transaction");
+        }
+    }
 
     return (
         <div className='container'>
@@ -38,10 +95,10 @@ const Transaction = () => {
                                         {element.type === "income" ? "+" : "-"} &#x20B9;{element.amount}
                                     </td>
                                     <td className='d-flex text-center gap-3'>
-                                        <span className="me-3 pointer">
+                                        <span onClick={() => showModal(element._id)} className="me-3 pointer">
                                             <i className="fa-regular fa-pen-to-square"></i>
                                         </span>
-                                        <span className='pointer'>
+                                        <span className='pointer' onClick={() => handlingDelete(element._id)}>
                                             <i className="fa-solid fa-trash"></i>
                                         </span>
                                     </td>
@@ -52,6 +109,43 @@ const Transaction = () => {
 
                     </tbody>
                 </table>
+
+                <Modal
+                    title="Add New Transaction"
+                    closable={{ 'aria-label': 'Custom Close Button' }}
+                    open={isModalOpen}
+                    onCancel={handleCancel}
+                    footer={false}
+                >
+
+                    <Form form={form} layout='vertical' onFinish={handleSubmit}>
+                        <Form.Item label="Title" name="title">
+                            <Input type='text' />
+                        </Form.Item>
+
+                        <Form.Item label="Amount" name="amount">
+                            <Input type='text' />
+                        </Form.Item>
+
+                        <Form.Item label="Date" name="date">
+                            <Input type='date'/>
+                        </Form.Item>
+
+                        <Form.Item label="Category" name="category">
+                            <Input type='text' placeholder='salary, food, entertainment...' />
+                        </Form.Item>
+
+                        <Form.Item label="Type" name="type">
+                            <Select >
+                                <Select.Option value="income">Income</Select.Option>
+                                <Select.Option value="expense">Expense</Select.Option>
+                            </Select>
+                        </Form.Item>
+                        <Button type="primary" htmlType="submit">Submit</Button>
+                    </Form>
+
+                </Modal>
+
             </div>
         </div>
     )
